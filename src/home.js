31a542605c0c5f303cf8ed5f9830b360b12f3328
Nabil <nabilomi1@gmail.com>
2021-07-17 17:11:01 -0400

    manga cards for search

--- src/home.js
+++ src/home.js
@@ -25,25 +25,46 @@ function SiteHeader() {
 function SearchManga() {
     const [mangaTitle, setMangaTitle] = useState("");
     const [error, setError] = useState(false);
-    let mangaArray = [];
+    const [mangaArray, setMangaArray] = useState([]);
     // TODO: ADD A REQUEST ON MOUNT THAT QUERIES FOR A COUPLE EMPTY MANGAS TO FILL SPACE
     // TODO: ADD A FILTER BY TAG OPTION
-
-    const order = {
-        updatedAt: 'asc'
-    };
     async function sendSearchRequest(e) {
         e.preventDefault();
 
-        await axios.get('https://api.mangadex.org/manga', {
+        await axios({
+            method: "GET",
+            url: "https://api.mangadex.org/manga", // https://api.mangadex.org/manga
             params: {
                 limit: 50,
                 title: mangaTitle,
                 contentRating: ["safe"]
-            },
+            }
         }).then(response => {
             let responseArr = response.data.results;
-            console.log(responseArr[3].data);
+            /* 
+                Structure of the response: 
+                    responseArr[i].relationships:
+                        [0]: Author ID
+                        [1]: Artist ID
+                        [2]: Cover Art ID
+                    responseArr[i].data:
+                        id: Manga ID
+                        attributes:
+                            title: JP Title
+                            title.en: EN Title
+                            description: Description
+            */
+            let mangas = [];
+            for (let i = 0; i < responseArr.length; i++) {
+                mangas.push({
+                    title: responseArr[i].data.attributes.title.en,
+                    authorID: responseArr[i].relationships[0].id,
+                    artistID: responseArr[i].relationships[1].id,
+                    description: responseArr[i].data.attributes.description.en.toString().substring(0, 300).concat("..."),
+                    coverArtID: responseArr[i].relationships[2].id
+                });
+            }
+            setMangaArray(mangas);
         }).catch(err => {
             console.error(err);
             setError(true);
@@ -54,14 +75,48 @@ function SearchManga() {
         setError(false);
     }
 
-    return (
+    return (<>
         <div className="manga-search-container">
             <form onSubmit={sendSearchRequest}>
                 <input className="manga-submit" type="text" value={mangaTitle} onChange={(e) => setMangaTitle(e.target.value)} id="manga-search" placeholder="Search By Title!" />
                 {/* TODO: ADD SEARCH BUTTON TO MOBILE ONLY */}
                 {/* <button type="submit" id="submit">asd</button> */}
             </form>
-            {error ? <p>An error has ocurred! Check console for more details. <button onClick={clearError}><strong>Clear</strong></button></p> : <></>}
+        </div>
+
+        <div className="search-results">
+            <MangaList mangaArray={mangaArray} />
+            {error ? <p className="submit-error">An error has ocurred! Check console for more details. <button onClick={clearError}><strong>Clear</strong></button></p> : <></>}
+        </div>
+    </>)
+}
+
+function MangaList(props) {
+    const mangas = props.mangaArray;
+    // console.log(mangas);
+    return (
+        <ul>
+            {mangas && mangas.map(manga => <MangaCard key={manga.title} manga={manga} />)}
+        </ul>
+    )
+}
+
+function MangaCard(props) {
+    let {
+        title,
+        authorID,
+        artistID,
+        description,
+        coverArtID
+    } = props.manga;
+    return (
+        <div className="manga-card">
+            <p>{title}</p>
+            <p>{description}</p>
+            {/* <p>Author ID: {authorID}</p>
+            <p>Artist ID: {artistID}</p>
+            <p>Cover Art ID: {coverArtID}</p> */}
         </div>
     )
 }
+
